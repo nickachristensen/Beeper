58d5494488d5eff1be6c1005e545abf455453c4f
Nicholas A Christensen <nickachristensen@gmail.com>
2021-10-27 17:38:05 -0400

    close modal functionality

--- client/src/Modal/index.js
+++ client/src/Modal/index.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import { Background, Content, HeaderRow } from "./Modal.styles";
 
 
 
-const Modal = ({ isOpen, close, children }) => {
-    if (!isOpen) return null;
 
+const Modal = ({ isOpen, close, children }) => {
+    
+    git const contentRef = useRef();
+
+    useEffect(() => {
+        if(!isOpen) return
+
+        function listener(event) {
+            if(!contentRef.current.contains(event.target)) {
+                close();
+            }
+        }
+        window.addEventListener("click", listener);
+        return () => {
+            window.removeEventListener("click", listener);
+        }
+    }, [isOpen]);
+
+    if (!isOpen) return null;
     return <Background>
-        <Content>
-            <HeaderRow>
-                {/* <img src={closeIcon} alt="close icon" /> */}
-            </HeaderRow>
-            {children}</Content>
+        <Content ref={contentRef}>
+            {children}
+        </Content>
     </Background>
 };
 
