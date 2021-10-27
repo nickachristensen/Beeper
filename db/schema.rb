9e97c477cbb0382547b8dc8538e622228559ee29
Nicholas A Christensen <nickachristensen@gmail.com>
2021-10-27 15:36:38 -0400

    fixed database

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2021_10_26_232118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
