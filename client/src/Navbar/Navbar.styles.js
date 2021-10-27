3e5bdbbde6fd3d2bee87712045225156ba5ed505
Nicholas A Christensen <nickachristensen@gmail.com>
2021-10-27 17:46:11 -0400

    added post moew form to modal

--- client/src/Navbar/Navbar.styles.js
+++ client/src/Navbar/Navbar.styles.js
@@ -33,3 +33,81 @@ export const LogoImg = styled.img`
     width: 150px;
   }
 `;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  background-color: transparent;
+  padding: 2rem;
+  box-shadow: 0px 1px 5px rgb(0 0 0 / 10%);
+  border-radius: 5px;
+  transition: 0.8s ease;
+  
+  p {
+    font-size: 1.5rem;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #05245a;
+    font-weight: 900;
+    margin-bottom: 1rem;
+  }
+`;
+
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 1rem 0rem;
+
+  label {
+    margin-bottom: 0.45rem;
+    color: #000000;
+    font-size: 12px;
+    font-family: Arial, Helvetica, sans-serif;
+    letter-spacing: 0.7px;
+    line-height: 1.3;
+  }
+
+  input {
+    padding: 0.55rem 0.4rem;
+    border: 1px solid rgb(184, 196, 194);
+    border-radius: 4px;
+    font-size: 14px;
+    outline: none;
+    transition: all 150ms ease-in-out 0s;
+    width: 85%;
+    background: #fff;
+  }
+
+  input::placeholder {
+    color: #b1b1b1;
+    width: 100%;
+    font-weight: unset;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  input:hover {
+    border-color: #dcdddd;
+  }
+`;
+
+export const Button = styled.div`
+  button {
+    border-radius: 4px;
+    background: #ef8181;
+    border: 1px solid #E63F3F;
+    color: #fff;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 500;
+    padding: 0.7rem 1.2rem;
+    outline: none;
+    cursor: pointer;
+    transition: 0.3s ease;
+    letter-spacing: 0.7px;
+    margin-top: 1rem;
+  }
+
+  button:hover {
+    background: #e53838;
+  }
+`;
--- client/src/Navbar/index.js
+++ client/src/Navbar/index.js
@@ -4,12 +4,35 @@ import CreateMeow from "../CreateMeow";
 import Modal from "../Modal"; 
 
 // Styles
-import { Wrapper, Content, LogoImg } from "./Navbar.styles";
+import { Wrapper, Content, LogoImg, Form, Input, Button } from "./Navbar.styles";
 import catLogo from "../images/catlogo.png";
 
 const Navbar = ({ handleLogout }) => {
   const [isOpen, setOpen] = useState(false);
-  console.log("isOpen", isOpen);
+  const [content, setContent] = useState([]);
+
+  function handleCreate(event) {
+    event.preventDefault();
+    fetch(`/posts`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content }),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setContent(data);
+          setContent([]);
+        });
+      } else {
+        res.json().then((errors) => {
+          console.error(errors);
+        });
+      }
+    });
+  }
 
 return (
 
@@ -18,10 +41,24 @@ return (
       <LogoImg src={catLogo} alt="catlogo" />
     </Content>
     <Content>
-    <button onClick={() => setOpen(isOpen => !isOpen)}>Open the Modal!</button>
+    <button onClick={() => setOpen(isOpen => !isOpen)}>Post Meow!</button>
     <Modal isOpen={isOpen} close={() => setOpen(false)}>
-      <h2>Hello from the Modal</h2>
-      <p>Im the modal!</p>
+      <Form>
+        <form onSubmit={handleCreate}>
+          <Input>
+            <label htmlFor="createmeow">CREATE MEOW:</label>
+            <input
+              type="text"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              name="content"
+              />
+          </Input>
+          <Button>
+            <button type="submit">✅</button>
+          </Button>
+        </form>
+      </Form>
     </Modal>
       <p>PROFILE</p>
       <p onClick={handleLogout}>LOGOUT</p>
