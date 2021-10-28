bd4debfcc4917553b1db65d0edc56eead2277aa3
Nicholas A Christensen <nickachristensen@gmail.com>
2021-10-28 11:11:23 -0400

    cleaned up delete post fetch a bit and it removed error on delete

--- client/src/Post/index.js
+++ client/src/Post/index.js
@@ -1,43 +1,29 @@
 import React, { useState } from "react";
+import { Avatar } from "@material-ui/core";
 import Modal from "../Modal";
 
 //Style
-import {
-  Wrapper,
-  Body,
-  Header,
-  Button,
-  Content,
-  Form,
-  Input,
-  Wrapper2,
-  Content2,
-  Body2,
-  Avatar,
-  Header2,
-} from "./Post.styles";
+import { Wrapper, Body, Header, Button, Content, Form, Input } from "./Post.styles";
 
 const Post = ({ post, onDelete, toggle, setToggle }) => {
   const { content, user } = post;
   const [isOpen, setOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState ({
     content: post.content,
-  });
+});
 
   function handleDelete() {
-    onDelete(post);
+    onDelete(post)
     fetch(`posts/${post.id}`, {
-      method: "DELETE",
+        method: "DELETE"
     })
-      .then((res) => {
-        res.json();
-      })
-      .then((res) => window.location.reload());
-  }
+    .then((res) => res.json())
+    .then(window.location.reload());
+}
 
-  function handleEdit(event) {
-    event.preventDefault();
+function handleEdit(event) {
+  event.preventDefault()
     const editPost = {
       content: formData.content,
     };
@@ -50,73 +36,68 @@ const Post = ({ post, onDelete, toggle, setToggle }) => {
     })
       .then((r) => r.json())
       .then(setToggle(!toggle));
-    setIsEditing((isEditing) => !isEditing);
-    window.location.reload();
+      setIsEditing((isEditing) => !isEditing);
+      window.location.reload();
   }
 
   function handleChange(event) {
     setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
+        ...formData,
+        [event.target.name]: event.target.value,
     });
-  }
+}
 
   return (
     <>
-      <Wrapper>
-        <Content onClick={() => setOpen((isOpen) => !isOpen)}>
+    <Wrapper>
+      <Content onClick={() => setOpen(isOpen => !isOpen)}>
+        <Body>
+          <Avatar src="https://www.wpbeginner.com/wp-content/uploads/2012/08/gravatarlogo.jpg" />
+            <h3>
+              {user.fullname} | {user.username}
+            </h3> 
+        <Header>
+          <p>{content}</p>
+        </Header>
+        </Body> 
+      </Content>
+      <Modal isOpen={isOpen} close={() => setOpen(false)}>
+        <Content>
           <Body>
             <Avatar src="https://www.wpbeginner.com/wp-content/uploads/2012/08/gravatarlogo.jpg" />
-            <h3>
-              {user.fullname} | @{user.username}
-            </h3>
+              <h3>
+                {user.fullname} | {user.username}
+              </h3>
           </Body>
+          {isEditing ?
+            <Form>
+              <form onSubmit = {handleEdit}>
+                <Input>
+                  <label htmlFor="editpost">EDIT POST:</label>
+                    <input 
+                      type = "text" 
+                      value={formData.content} 
+                      onChange={handleChange} 
+                      name="content" />
+                </Input>
+                <Button>
+                  <button type="submit">✅</button>
+                </Button>
+                </form>
+            </Form>
+        :
           <Header>
             <p>{content}</p>
           </Header>
+        }
         </Content>
-      </Wrapper>
-      <Wrapper2>
-        <Modal isOpen={isOpen} close={() => setOpen(false)}>
-          <Content2>
-            <Body2>
-              <Avatar src="https://www.wpbeginner.com/wp-content/uploads/2012/08/gravatarlogo.jpg" />
-              <h3>
-                {user.fullname} | @{user.username}
-              </h3>
-            </Body2>
-            {isEditing ? (
-              <Form>
-                <form onSubmit={handleEdit}>
-                  <Input>
-                    <label htmlFor="editpost">EDIT POST:</label>
-                    <textarea
-                      type="text"
-                      value={formData.content}
-                      onChange={handleChange}
-                      name="content"
-                    />
-                  </Input>
-                  <Button>
-                    <button type="submit">SAVE</button>
-                  </Button>
-                </form>
-              </Form>
-            ) : (
-              <Header2>
-                <p>{content}</p>
-              </Header2>
-            )}
-          </Content2>
-          <Button>
-            <button>Reply</button>
-            <button onClick={() => setIsEditing((isEditing) => !isEditing)}>
-              Edit
-            </button>
-            <button onClick={handleDelete}>Delete</button>
-          </Button>
-        </Modal>
-      </Wrapper2>
+      <Button>
+        <button>Reply</button>
+        <button onClick = {() => setIsEditing((isEditing) => !isEditing)}>Edit</button>
+        <button onClick = {handleDelete}>Delete</button>
+      </Button>
+      </Modal>
+    </Wrapper>
     </>
   );
 };
--- client/src/useBookSearch.js
+++ client/src/useBookSearch.js
@@ -1,39 +1,39 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
-import React from "react";
+// import React from "react";
 
-const useBookSearch = (query, pageNumber) => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [books, setBooks] = useState([]);
-  const [hasMore, setHasMore] = useState(false);
+// const useBookSearch = (query, pageNumber) => {
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(false);
+//   const [books, setBooks] = useState([]);
+//   const [hasMore, setHasMore] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    setError(false);
-    let cancel;
-    axios({
-      method: "GET",
-      url: "/posts",
-      params: { q: query, page: pageNumber },
-      cancelToken: new axios.CancelToken((c) => (cancel = c)),
-    })
-      .then((res) => {
-        setBooks((prevBooks) => {
-          return [...new Set([...prevBooks, res.data.map((b) => b)])];
-        });
-        setHasMore(res.data.length > 0);
-        setLoading(false);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        if (axios.isCancel(err)) return;
-        setError(true);
-      });
-    return () => cancel();
-  }, [query, pageNumber]);
-  return { loading, error, books, hasMore };
-};
+//   useEffect(() => {
+//     setLoading(true);
+//     setError(false);
+//     let cancel;
+//     axios({
+//       method: "GET",
+//       url: "/posts",
+//       params: { q: query, page: pageNumber },
+//       cancelToken: new axios.CancelToken((c) => (cancel = c)),
+//     })
+//       .then((res) => {
+//         setBooks((prevBooks) => {
+//           return [...new Set([...prevBooks, res.data.map((b) => b)])];
+//         });
+//         setHasMore(res.data.length > 0);
+//         setLoading(false);
+//         console.log(res.data);
+//       })
+//       .catch((err) => {
+//         if (axios.isCancel(err)) return;
+//         setError(true);
+//       });
+//     return () => cancel();
+//   }, [query, pageNumber]);
+//   return { loading, error, books, hasMore };
+// };
 
-export default useBookSearch;
+// export default useBookSearch;
